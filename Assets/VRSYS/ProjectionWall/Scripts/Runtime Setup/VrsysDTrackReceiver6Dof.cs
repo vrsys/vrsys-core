d3ccab55c997a7604eefc228f27a487d8a2f15d6
Lucky Chandrautama <lucky.chandrautama@uni-weimar.de>
2024-06-24 16:05:30 +0200

    Dtrack extension and mesh adjustment

---
+++ Assets/VRSYS/ProjectionWall/Scripts/Runtime Setup/VrsysDTrackReceiver6Dof.cs
@@ -0,0 +1,117 @@
+// VRSYS plugin of Virtual Reality and Visualization Research Group (Bauhaus University Weimar)
+//  _    ______  _______  _______
+// | |  / / __ \/ ___/\ \/ / ___/
+// | | / / /_/ /\__ \  \  /\__ \ 
+// | |/ / _, _/___/ /  / /___/ / 
+// |___/_/ |_|/____/  /_//____/  
+//
+//  __                            __                       __   __   __    ___ .  . ___
+// |__)  /\  |  | |__|  /\  |  | /__`    |  | |\ | | \  / |__  |__) /__` |  |   /\   |  
+// |__) /~~\ \__/ |  | /~~\ \__/ .__/    \__/ | \| |  \/  |___ |  \ .__/ |  |  /~~\  |  
+//
+//       ___               __                                                           
+// |  | |__  |  |\/|  /\  |__)                                                          
+// |/\| |___ |  |  | /~~\ |  \                                                                                                                                                                                     
+//
+// Copyright (c) 2022 Virtual Reality and Visualization Research Group
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//-----------------------------------------------------------------
+//   Authors:        Sebastian Muehlhaus, Andre Kunert, Lucky Chandrautama
+//   Date:           2024
+//-----------------------------------------------------------------
+//
+// Implemented based on ART DTRACK Plugin for Unity Game Engine: DTrackReceiver6Dof.cs
+// This enables ART Tracking Data to be applied as local transform as opposed to the default
+// global transform.
+// 
+
+using System;
+using UnityEngine;
+using VRSYS.Core.Logging;
+using DTrackSDK;
+using DTrack;
+using DTrack.Util;
+
+
+namespace VRSYS.ProjectionWall
+{
+	/// <summary>
+	/// Implemented based on ART DTRACK Plugin for Unity Game Engine: DTrackReceiver6Dof.cs.
+	/// This enables ART Tracking Data to be applied as local transform as opposed to the default
+	/// global transform. This works in most use cases specifically for Group Navigation that utilizes
+	/// a parent navigation platform for all Projection Wall Users. If ART Tracking data must be 
+	/// applied to the global transformation, use DTrackReceiver6Dof.cs instead.
+	/// </summary>
+	public class VrsysDTrackReceiver6Dof : DTrackReceiver
+	{
+		[Tooltip("Standard Body ID as seen in DTRACK")]
+		public int bodyId;
+
+		[Tooltip("Update position of this GameObject")]
+		public bool applyPosition = true;
+		[Tooltip("Update rotation of this GameObject")]
+		public bool applyRotation = true;
+
+		void OnEnable()
+		{
+			Register();
+		}
+
+		void OnDisable()
+		{
+			Unregister();
+		}
+
+
+		void Update()
+		{
+			DTrackSDK.Frame frame = GetDTrackFrame();  // ensures data integrity against DTrack class
+			if (frame == null) return;  // no new tracking data
+			if (frame.Bodies == null) return;
+
+			try
+			{
+                DTrackBody dtBody;
+
+				if (frame.Bodies.TryGetValue(bodyId - 1, out dtBody))
+				{
+					if (dtBody.IsTracked)
+					{
+						if (applyPosition)
+						{
+							transform.localPosition = Converter.PositionToUnity(dtBody.Loc);
+						}
+
+						if (applyRotation)
+						{
+							transform.localRotation = Converter.RotationToUnity(dtBody.Quaternion);
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				ExtendedLogger.LogError(GetType().Name, "Error while moving object: " + e.ToString());
+			}
+		}
+	}
+
+}
+
+

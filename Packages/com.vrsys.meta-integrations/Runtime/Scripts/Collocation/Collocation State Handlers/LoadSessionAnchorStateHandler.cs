8a2e379a975b6092e10905fb708704110f06f084
Tony Zöppig <tonyjan98@gmail.com>
2026-01-22 09:25:54 +0100

    Load session anchor state

--- Packages/com.vrsys.meta-integrations/Runtime/Scripts/Collocation/Collocation State Handlers/LoadSessionAnchorStateHandler.cs
+++ Packages/com.vrsys.meta-integrations/Runtime/Scripts/Collocation/Collocation State Handlers/LoadSessionAnchorStateHandler.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Task = System.Threading.Tasks.Task;
 
 namespace VRSYS.Meta.Collocation
 {
@@ -16,12 +19,103 @@ namespace VRSYS.Meta.Collocation
 
         public override void StartState()
         {
-            
+            LoadSessionAnchor();
         }
 
         protected override void EndState()
         {
+            CollocationStateMessage stateMessage = new CollocationStateMessage(State, CollocationStateStatus.Success,
+                "Successfully loaded and localized session anchor.");
             
+            // TODO: enter alignment state
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private async void LoadSessionAnchor()
+        {
+            bool isFirstTry = _retryCount == 0;
+
+            CollocationStateStatus status = isFirstTry ? CollocationStateStatus.Started : CollocationStateStatus.Retry;
+            string message = isFirstTry ? "Starting to load session anchor." : "Retry to load session anchor.";
+
+            CollocationStateMessage stateMessage = new CollocationStateMessage(State, status, message);
+            _manager.BroadcastState(stateMessage);
+
+            var unboundAnchors = new List<OVRSpatialAnchor.UnboundAnchor>();
+            var loadResult =
+                await OVRSpatialAnchor.LoadUnboundSharedAnchorsAsync(_manager.JoinedSessionData.AdvertisementUuid,
+                    unboundAnchors);
+
+            if (!loadResult.Success || unboundAnchors.Count == 0)
+            {
+                if (_retryCount == _manager.MaxRetries)
+                {
+                    stateMessage = new CollocationStateMessage(State, CollocationStateStatus.Failed,
+                        $"Failed to load session anchor. Anchors found: {unboundAnchors.Count}, Result: {loadResult.Status}");
+                    _manager.BroadcastState(stateMessage);
+                    return;
+                }
+
+                _retryCount++;
+
+                stateMessage = new CollocationStateMessage(State, CollocationStateStatus.Retry,
+                    $"Failed to load session anchor. Retrying in {_manager.RetryTime} seconds.");
+                _manager.BroadcastState(stateMessage);
+
+                await Task.Delay((int)(_manager.RetryTime * 1000));
+                
+                LoadSessionAnchor();
+                
+                return;
+            }
+            
+            LocalizeAnchor(unboundAnchors[0]);
+        }
+
+        private async void LocalizeAnchor(OVRSpatialAnchor.UnboundAnchor unboundAnchor)
+        {
+            bool isFirstTry = _retryCount == 0;
+
+            CollocationStateStatus status = isFirstTry ? CollocationStateStatus.Running : CollocationStateStatus.Retry;
+            string message = isFirstTry ? "Starting to localize session anchor." : "Retry to localize session anchor.";
+
+            CollocationStateMessage stateMessage = new CollocationStateMessage(State, status, message);
+            _manager.BroadcastState(stateMessage);
+
+            if (await unboundAnchor.LocalizeAsync())
+            {
+                stateMessage = new CollocationStateMessage(State, CollocationStateStatus.Running,
+                    "Session anchor successfully localized. Starting binding of anchor.");
+                _manager.BroadcastState(stateMessage);
+
+                var spatialAnchor = Object.Instantiate(_manager.AnchorPrefab);
+                unboundAnchor.BindTo(spatialAnchor);
+                
+                _manager.SetCurrentAnchor(spatialAnchor);
+                
+                EndState();
+                
+                return;
+            }
+
+            if (_retryCount == _manager.MaxRetries)
+            {
+                stateMessage = new CollocationStateMessage(State, CollocationStateStatus.Failed,
+                    "Failed to localize session anchor.");
+                _manager.BroadcastState(stateMessage);
+                return;
+            }
+
+            _retryCount++;
+
+            stateMessage = new CollocationStateMessage(State, CollocationStateStatus.Retry,
+                $"Failed to localize session anchor. Retry in {_manager.RetryTime} seconds.");
+            _manager.BroadcastState(stateMessage);
+            
+            LocalizeAnchor(unboundAnchor);
         }
 
         #endregion

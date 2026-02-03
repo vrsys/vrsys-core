ced9fd17f8e13412c370ca9bb8b7505e3d01838d
Karoline Brehm <karoline.engel.brehm@uni-weimar.de>
2026-02-03 16:13:19 +0100

    Draft Scene Alignment to LocalUser using arbitrary spatial anchor _currentAnchor.

--- Packages/com.vrsys.meta-integrations/Runtime/Scripts/Collocation/Collocation State Handlers/AligningToAnchorStateHandler.cs
+++ Packages/com.vrsys.meta-integrations/Runtime/Scripts/Collocation/Collocation State Handlers/AligningToAnchorStateHandler.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using System.Collections.Generic;
 using UnityEngine.XR;
-using 
+using VRSYS.Core.Networking;
 
 namespace VRSYS.Meta.Collocation
 {
     public class AligningToAnchorStateHandler : CollocationStateHandler
     {
+        private XRInputSubsystem _xrInputSubsystem;
         public AligningToAnchorStateHandler(CollocationManager manager) : base(manager)
         {
             State = CollocationState.AligningToAnchor;
@@ -17,31 +16,77 @@ namespace VRSYS.Meta.Collocation
 
         public override void StartState()
         {
-            throw new System.NotImplementedException();
+            InitializeXRInputSubsystem();
+            AlignScene();
         }
 
         protected override void EndState()
         {
-            throw new System.NotImplementedException();
+            _manager.BroadcastState(new CollocationStateMessage(State, CollocationStateStatus.Running,
+                "Removing callback to TrackingOriginUpdates and resetting scene alignment."));
+            _xrInputSubsystem.trackingOriginUpdated -= OnTrackingOriginUpdated;
+            // TODO: Is it desired to reset scene alignment here?
+            ResetSceneAlignment();
         }
         
-        private XRInputSubsystem _xrInputSubsystem;
-        
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private void InitializeXRInputSubsystem()
         {
+            _manager.BroadcastState(new CollocationStateMessage(State, CollocationStateStatus.Started,
+                "Setting up callback to TrackingOriginUpdates."));
+            
             List<XRInputSubsystem> subsystems = new List<XRInputSubsystem>();
-            SubsystemManager.GetSubsystems(subsystems); //Returns active Subsystems of XRInputSubsystem type
+            SubsystemManager.GetSubsystems(subsystems); // Returns active Subsystems of XRInputSubsystem type
             if (subsystems.Count > 0)
             {
                 _xrInputSubsystem = subsystems[0];
                 _xrInputSubsystem.trackingOriginUpdated += OnTrackingOriginUpdated;
+                _manager.BroadcastState(new CollocationStateMessage(State, CollocationStateStatus.Running,
+                    "Subscribed to TrackingOriginUpdates."));
+            }
+            else
+            {
+                _manager.BroadcastState(new CollocationStateMessage(State, CollocationStateStatus.Failed,
+                    "No XR Subsystem found. Cannot subscribe to TrackingOriginUpdates."));
             }
         }
 
+        /// <summary>
+        /// Aligns scene to the current anchor held by Colocation Manager
+        /// </summary>
+        public void AlignScene()
+        {
+            var origin = NetworkUser.LocalInstance.transform; // XR Origin
+            var alignmentAnchor = _manager.CurrentAnchor;
+            
+            _manager.BroadcastState(new CollocationStateMessage(State, CollocationStateStatus.Running,
+                $"Aligning scene with origin {origin.position} to current anchor " 
+                + $"with position {alignmentAnchor.transform.position}"));
+            
+            Matrix4x4 O = Matrix4x4.TRS(
+                origin.position,
+                origin.rotation,
+                Vector3.one
+            );
+
+            Matrix4x4 A = Matrix4x4.TRS(
+                alignmentAnchor.transform.position,
+                Quaternion.LookRotation(alignmentAnchor.transform.forward),
+                Vector3.one);
+
+            var alignmentMatrix = O * A.inverse;
+
+            origin.position = alignmentMatrix.GetColumn(3);
+            origin.rotation = alignmentMatrix.rotation;
+            origin.localScale = alignmentMatrix.lossyScale;
+
+            // origin.position = a.transform.position;
+            // origin.LookAt(b.transform.position, Vector3.up);
+        }
+        
         private void OnTrackingOriginUpdated(XRInputSubsystem obj)
         {
-            Debug.Log("Tracking Origin Updated");
+            _manager.BroadcastState(new CollocationStateMessage(State, CollocationStateStatus.Started,
+                $"TrackingOriginUpdated. Realigning scene..."));
             _manager.StartCoroutine(ResetAlignmentNextFrame());
         }
         
@@ -54,48 +99,9 @@ namespace VRSYS.Meta.Collocation
         
         public void ResetSceneAlignment()
         {
-            var origin = FindAnyObjectByType<XROrigin>().transform;
+            var origin = NetworkUser.LocalInstance.transform;
             origin.position = Vector3.zero;
             origin.rotation = Quaternion.identity;
-        }
-
-        /// <summary>
-        /// Aligns scene to current tracked anchor position
-        /// </summary>
-        public void AlignScene()
-        {
-            Debug.Log("Align Scene");
-            if (!AlignmentAnchor.IsAlignmentValid())
-            {
-                Debug.Log("No alignment nodes specified");
-                return;
-            }
-            var result = AlignmentAnchor.GetAlignmentAnchors();
-            if (result is (ARAnchor a, ARAnchor b))
-            {
-                var origin = FindAnyObjectByType<XROrigin>().transform;
-                Debug.Log($"Anchor Pos {a.transform.position} Origin Position {origin.position}");
-
-                Matrix4x4 O = Matrix4x4.TRS(
-                    origin.position,
-                    origin.rotation,
-                    Vector3.one
-                );
-
-                Matrix4x4 A = Matrix4x4.TRS(
-                    a.transform.position,
-                    Quaternion.LookRotation(b.transform.position - a.transform.position),
-                    Vector3.one);
-
-                var alignmentMatrix = O * A.inverse;
-
-                origin.position = alignmentMatrix.GetColumn(3);
-                origin.rotation = alignmentMatrix.rotation;
-                origin.localScale = alignmentMatrix.lossyScale;
-
-                // origin.position = a.transform.position;
-                // origin.LookAt(b.transform.position, Vector3.up);
-            }
         }
     }
 }
